4db02b974e2e7e2d900a9f76245a56f8747a54dd
Mauricio Daniel Gomez Torres <mauriciodanielgomez_torres@apple.com>
2020-01-24 16:05:24 -0800

    Fixes

--- data/sql/V1__initialize/V1.6__add_user_columns.sql
+++ data/sql/V1__initialize/V1.6__add_user_columns.sql
@@ -1,7 +1,7 @@
-alter table talachitas.user add verify_email bigint;
+alter table talachitas.users add verify_email bigint;
 
-alter table talachitas.user add verify_phone bigint;
+alter table talachitas.users add verify_phone bigint;
 
-alter table talachitas.user add retry_email bigint;
+alter table talachitas.users add retry_email bigint;
 
-alter table talachitas.user add retry_phone bigint;
+alter table talachitas.users add retry_phone bigint;
